58ec1c1ca43e3679bffef2f8cdd5132691928dee
Nate-Workspace <nathanisrael.jobs@gmail.com>
2025-08-13 12:03:03 +0300

    fix: back again

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,7 +14,7 @@ interface Director extends Teacher {
     numberOfReports: number;
 }
 
-interface printTeacher {
+interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
@@ -49,7 +49,7 @@ const director1: Directors = {
 console.log(director1);
 
 // Task 3
-const printTeacher: printTeacher = (firstName, lastName) => {
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
